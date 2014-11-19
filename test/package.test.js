e18670d34dc6d150c7a429bd4a10fb35930cf884
Jared Hanson <jaredhanson@gmail.com>
2014-11-18 16:30:33 -0800

    Fix tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,11 @@
 /* global describe, it, expect */
 
-var pkg = require('..');
+var sd = require('..');
 
 describe('bixby-sd', function() {
   
-  it('should export object', function() {
-    expect(pkg).to.be.an('object');
+  it('should export a function', function() {
+    expect(sd).to.be.an('function');
   });
   
 });
