d806075e5c218d76365318cab552a6577eea7ba9
Jared Hanson <jaredhanson@gmail.com>
2018-12-02 15:45:26 -0800

    Use apply to forward call to registry.

--- app/main.js
+++ app/main.js
@@ -2,15 +2,11 @@ exports = module.exports = function(registry) {
   var api = {};
   
   api.resolve = function(type, domain, rrtype, cb) {
-    registry.resolve(type, domain, rrtype, function(err, records) {
-      cb(err, records);
-    });
+    registry.resolve.apply(registry, arguments);
   };
   
   api.resolveSrv = function(type, domain, cb) {
-    registry.resolveSrv(type, domain, function(err, records) {
-      cb(err, records);
-    });
+    registry.resolveSrv.apply(registry, arguments);
   };
   
   return api;
