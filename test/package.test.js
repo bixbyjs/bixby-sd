38226bdb39865ed1a57aa8b416e204409c08a8e3
Jared Hanson <jaredhanson@gmail.com>
2019-05-09 18:19:55 -0700

    Update tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,31 @@
 /* global describe, it, expect */
 
-var sd = require('..');
+var expect = require('chai').expect;
+var sinon = require('sinon');
 
-describe('bixby-sd', function() {
+
+describe('bixby-ns', function() {
   
-  it('should export a function', function() {
-    expect(sd).to.be.an('function');
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('sd');
+      
+      expect(json.assembly.components).to.have.length(1);
+      expect(json.assembly.components).to.include('main');
+    });
   });
   
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
+  });
+  
+});
+
+
+afterEach(function() {
+  sinon.restore();
 });
