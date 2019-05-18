302f81e006bb1548a241c2d7440ba58c64a99003
Jared Hanson <jaredhanson@gmail.com>
2019-05-18 09:07:27 -0700

    Add tests.

--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,10 @@
 /* global describe, it, expect */
 
+var $require = require('proxyquire');
+var expect = require('chai').expect;
+var sinon = require('sinon');
 var factory = require('../app/main');
+var API = require('../lib/api');
 
 
 describe('main', function() {
@@ -12,6 +16,18 @@ describe('main', function() {
   it('should be annotated', function() {
     expect(factory['@implements']).to.deep.equal([ 'http://i.bixbyjs.org/ns', 'http://i.bixbyjs.org/sd' ]);
     expect(factory['@singleton']).to.equal(true);
+  });
+  
+  it('should create API', function() {
+    var _switch = new Object();
+    
+    var APIStub = sinon.stub().returns(sinon.createStubInstance(API));
+    var api = $require('../app/main',
+      { '../lib/api': APIStub }
+    )(_switch);
+    
+    expect(APIStub).to.have.been.calledWithExactly(_switch).and.calledWithNew;
+    expect(api).to.be.an.instanceof(API);
   });
   
 });
