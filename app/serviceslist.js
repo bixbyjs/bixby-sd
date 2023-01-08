07929231e1f3c2dbc13f19c14ffad6a2b1d44768
Jared Hanson <jaredhanson@gmail.com>
2023-01-07 16:07:04 -0800

    Add services list.

---
+++ app/serviceslist.js
@@ -0,0 +1,33 @@
+var List = require('../lib/serviceslist');
+
+exports = module.exports = function(C, Logger) {
+  var list = new List();
+  
+  return Promise.resolve(list)
+    .then(function(list) {
+      var components = C.components()
+        , component, i, len;
+      
+      for (i = 0, len = components.length; i < len; ++i) {
+        component = components[i];
+        if (component.a['@service']) {
+          list.add({
+            name: component.a['@service'],
+            port: component.a['@port']
+          });
+          
+          // TODO: Improve log message
+          Logger.info('Registered service name: ' + component.a['@service']);
+        }
+      }
+      
+      return list;
+    });
+};
+
+exports['@implements'] = 'http://i.bixbyjs.org/ns/ServicesList';
+exports['@singleton'] = true;
+exports['@require'] = [
+  '!container',
+  'http://i.bixbyjs.org/Logger'
+];
