12cd8d137bf965871bba4fc15deb55cd6f3fcfde
Jared Hanson <jaredhanson@gmail.com>
2024-08-30 16:37:55 -0700

    Add local/resolver component.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- `local/resolver` component implementing `http://i.bixbyjs.org/ns/LocalResolver`
+interface, intended for bootstrapping network-based service discovery services.
 
 ## [0.0.6] - 2024-08-30
 ### Changed
---
+++ app/local/resolver.js
@@ -0,0 +1,17 @@
+var Switch = require('../../lib/switch');
+
+exports = module.exports = function(localhost, environ) {
+  
+  var nss = new Switch();
+  nss.use('localhost.', localhost, true);
+  nss.use('localhost.', environ); // TODO: shoudl this be localhost?
+  
+  return nss;
+}
+
+exports['@implements'] = 'http://i.bixbyjs.org/ns/LocalResolver';
+exports['@singleton'] = true;
+exports['@require'] = [
+  '../resolver/localhost',
+  '../resolver/environ'
+];
