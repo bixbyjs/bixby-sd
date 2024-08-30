f3b05289edfd23a9152c1f90fe21cb06f1a95599
Jared Hanson <jaredhanson@gmail.com>
2024-08-30 11:13:50 -0700

    Add tests.

--- test/resolver/localhost.test.js
+++ test/resolver/localhost.test.js
@@ -89,4 +89,42 @@ describe('resolver/localhost', function() {
     });
   }); // should error when connection to port cannot be established
   
+  it('should error when service is not found in registry', function(done) {
+    var net = {
+      createConnection: sinon.spy(function(port) {
+        var socket = new events.EventEmitter();
+        process.nextTick(function() {
+          socket.remotePort = port;
+          socket.emit('connect');
+        });
+        return socket;
+      })
+    }
+    var LocalhostResolver = $require('../../lib/localhostresolver', {
+      'net': net
+    });
+    var factory = $require('../../app/resolver/localhost', {
+      '../../lib/localhostresolver': LocalhostResolver
+    });
+    
+    
+    var registry = new Object();
+    registry.get = sinon.stub().returns(undefined);
+    
+    var resolver = factory(registry);
+    resolver.resolve('_postgresql._tcp', 'SRV', function(err, records) {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('querySrv ENOTFOUND _postgresql._tcp');
+      expect(err.code).to.equal('ENOTFOUND');
+      
+      expect(registry.get.callCount).to.equal(1);
+      expect(registry.get.getCall(0).args[0]).to.equal('postgresql');
+      expect(registry.get.getCall(0).args[1]).to.equal('tcp');
+      expect(net.createConnection.callCount).to.equal(0);
+      
+      expect(records).to.be.undefined;
+      done();
+    });
+  }); // should error when connection to port cannot be established
+  
 });
