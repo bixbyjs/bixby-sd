c27d59a4a2f97e8a97c0e5c23ee4ceb075d97528
Jared Hanson <jaredhanson@gmail.com>
2024-08-30 11:03:04 -0700

    Clean up tests.

--- test/resolver/localhost.test.js
+++ test/resolver/localhost.test.js
@@ -11,35 +11,39 @@ describe('resolver/localhost', function() {
   
   it('should resolve to value of environment variable matching service name', function(done) {
     var net = {
-      createConnection: sinon.spy(function() {
-        console.log('CREATE CONNECTION!');
-      
-        var emitter = new events.EventEmitter();
+      createConnection: sinon.spy(function(port) {
+        var socket = new events.EventEmitter();
         process.nextTick(function() {
-          emitter.emit('connect');
+          socket.remotePort = port;
+          socket.emit('connect');
         });
-        return emitter;
+        return socket;
       })
     }
-    
     var LocalhostResolver = $require('../../lib/localhostresolver', {
       'net': net
     });
-    
     var factory = $require('../../app/resolver/localhost', {
       '../../lib/localhostresolver': LocalhostResolver
     });
     
     
     var registry = new Object();
-    registry.get = sinon.stub().returns({ port: 6432, protocol: 'tcp' })
+    registry.get = sinon.stub().returns({ port: 5432, protocol: 'tcp' });
     
     var resolver = factory(registry);
     resolver.resolve('_postgresql._tcp', 'SRV', function(err, records) {
       expect(err).to.be.null;
+      
+      expect(registry.get.callCount).to.equal(1);
+      expect(registry.get.getCall(0).args[0]).to.equal('postgresql');
+      expect(registry.get.getCall(0).args[1]).to.equal('tcp');
+      expect(net.createConnection.callCount).to.equal(1);
+      expect(net.createConnection.getCall(0).args[0]).to.equal(5432);
+      
       expect(records).to.deep.equal([ {
         name: 'localhost',
-        port: undefined,
+        port: 5432,
         priority: 1,
         weight: 1
       } ]);
