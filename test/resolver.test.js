291a85f1e39a232365bb53373c67418fbd47a217
Jared Hanson <jaredhanson@gmail.com>
2024-09-29 15:35:15 -0700

    Add tests.

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -35,21 +35,22 @@ describe('resolver', function() {
   it('should create resolver with support for localhost domain', function(done) {
     var container = new Object();
     container.components = sinon.stub().returns([]);
-    var portResolver = new Object();
     var environResolver = new Object();
+    var portResolver = new Object();
     
     var ResolverStub = sinon.stub().returns(sinon.createStubInstance(Switch));
     var factory = $require('../app/resolver',
       { '../lib/switch': ResolverStub }
     );
-    var promise = factory(container, portResolver, environResolver, _logger);
     
-    factory(container, _services, _logger)
+    factory(container, environResolver, portResolver, _logger)
       .then(function(resolver) {
         expect(resolver).to.be.an.instanceof(Switch);
         expect(resolver.use.callCount).to.equal(2);
-        expect(resolver.use.getCall(0).args[0]).to.equal('localhost.');
+        expect(resolver.use.getCall(0).args[0]).to.equal('localhost');
         expect(resolver.use.getCall(0).args[1]).to.equal(environResolver);
+        expect(resolver.use.getCall(1).args[0]).to.equal('localhost');
+        expect(resolver.use.getCall(1).args[1]).to.equal(portResolver);
         done();
       })
       .catch(done);
